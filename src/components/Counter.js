5fba51bec96d932e70dd9fb3109fc3cfc09c7fcd
Shajadul <shajadulrahe@gmail.com>
2022-12-13 11:53:13 +0600

    Feat: use redux store from actions

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,23 @@
-import classes from './Counter.module.css';
-
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
 const Counter = () => {
   const toggleCounterHandler = () => {};
-
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
